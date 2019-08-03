938d112a58c56811372ee1f3b4f50f6bf9df83e1
Zhengyi Zheng <zhengzhengyi1@gmail.com>
2019-08-03 13:34:03 -0700

    getcha final decoration

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,6 +4,7 @@ import './card.styles.css';
 export const Card = props => (
     <div className="card-container">
         <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`} />
-        <h1>{props.monster.name}</h1>
+        <h2>{props.monster.name}</h2>
+        <h3>{props.monster.email}</h3>
     </div>
 )
