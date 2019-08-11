26d9f5a136f00fb6e1d06fb1d70d49e73e7583fd
Zhengyi Zheng <zhengzhengyi1@gmail.com>
2019-08-10 18:39:37 -0700

    Updates

---
+++ precache-manifest.8bff16df1fdd5f9347eb7d2dbabe5f31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071cca349a7c1dec6e8bd8a9308150f6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f774d13ddf9fcf5b3676",
+    "url": "/monsters-rolodex/static/css/main.b97256a3.chunk.css"
+  },
+  {
+    "revision": "db3f69d6ef0ddbdf8d05",
+    "url": "/monsters-rolodex/static/js/2.d1756511.chunk.js"
+  },
+  {
+    "revision": "f774d13ddf9fcf5b3676",
+    "url": "/monsters-rolodex/static/js/main.6c35faf9.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
